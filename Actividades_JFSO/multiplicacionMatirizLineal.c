1dd7436469b5983e1d9227e0412f6ae8e5dd2a47
Juzarx <jsalazarojeda@gmail.com>
2025-05-27 13:36:14 -0600

    mayo27

--- Actividades_JFSO/multiplicacionMatirizLineal.c
+++ Actividades_JFSO/multiplicacionMatirizLineal.c
@@ -1,7 +1,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-#define N 5
+#define N 100
 
 int i = 0;
 int j = 0;
@@ -10,30 +10,34 @@ int k = 0;
 int A[N][N];
 int B[N][N];
 int C[N][N];
+int n = 1;
 
 void llenado(){
 
     for(i = 0; i < N; i ++){
         for (j = 0; j<N; j++){
-            A[i][j] = 1;
-            B[i][j] = 1;
+            A[i][j] = n;
+            B[i][j] = n;
+            n++;
         }
     }
 }
 
 
-void main(){
+int main(){
     llenado();
 
-    for(j = 0; j < N; j++){
-        for(i = 0; i < N; i++){
-        C[i][j] = 0;
-        for(k = 0; k < N; k++){
-        C[i][j] += A[i][k] * B[k][j];
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            C[i][j] = 0;
+            for(k = 0; k < N; k++){
+            C[i][j] += A[i][k] * B[k][j];
         }
-        printf("%d", C[i][j]);
+        printf("[%d]", C[i][j]);
     }
     printf("\n");
 }
+
+return 0;
 
 }
